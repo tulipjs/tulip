7a55973161d14e371d68305d6bd5e453cb3bb25c
pagoru <pagoru@gmail.com>
2024-06-16 17:37:59 +0200

    fix #7

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from "./mutables.utils";
 export * from "./display-object.utils";
 export * from "./shapes.utils";
-export * from "./ticker.utils";
 export * from "./degree.utils";
 export * from "./random.utils";
