9ca86c2c3f2f590364c292239b26d9dc1ed571d0
pagoru <pagoru@gmail.com>
2024-06-24 00:40:54 +0200

    wip #13

--- src/components/core/body.sub-component.test.ts
+++ src/components/core/body.sub-component.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "@jest/globals";
 import { body } from "./body.sub-component";
 import { Shape } from "../../enums";
+import { degreesToRadians } from "../../utils";
 
 describe("sub-components", () => {
   describe("core", () => {
@@ -56,14 +57,54 @@ describe("sub-components", () => {
           .shapes.find(({ id }) => id === shapeId);
         expect(foundShape).toBe(undefined);
       });
-      test.todo("setPosition(...)");
-      test.todo("getPosition()");
-      test.todo("setAngle(...)");
-      test.todo("getAngle(...)");
-      test.todo("addForce(...)");
-      test.todo("addForceX(...)");
-      test.todo("addForceY(...)");
-      test.todo("$getContactBody(...)");
+      test("setPosition(...) to assign inverted positions on the body", () => {
+        $body.setPosition({ x: 123, y: 321 });
+        expect($body.getPosition()).toStrictEqual({
+          x: 123,
+          y: 321,
+        });
+        expect($body.$getBody().position).toStrictEqual([-123, -321]);
+      });
+      test("setAngle(...) and check if radians are correct", () => {
+        $body.setAngle(46);
+        expect($body.getAngle()).toStrictEqual(46);
+        expect($body.$getBody().angle).toStrictEqual(degreesToRadians(46));
+      });
+      test("addForce(...) to be changed on the body", () => {
+        $body.addForce({ x: 10, y: 20 });
+        expect($body.$getBody().force).toStrictEqual([10, 20]);
+      });
+      test("addForceX(...) to be changed on the body", () => {
+        $body.addForceX(12);
+        expect($body.$getBody().force[0]).toStrictEqual(12);
+      });
+      test("addForceY(...) to be changed on the body", () => {
+        $body.addForceY(56);
+        expect($body.$getBody().force[1]).toStrictEqual(56);
+      });
+      test("setMass(...) to be changed on the body", () => {
+        $body.setMass(555);
+        expect($body.$getBody().mass).toStrictEqual(555);
+      });
+      test("$getContactBody(...) check that the contact material is valid", () => {
+        const $newBody = body();
+
+        const contactMaterial = $body.$getContactBody($newBody);
+
+        expect(contactMaterial.materialA).toStrictEqual($body.$getMaterial());
+        expect(contactMaterial.materialB).toStrictEqual(
+          $newBody.$getMaterial(),
+        );
+
+        expect(contactMaterial.surfaceVelocity).toStrictEqual(1);
+        expect(contactMaterial.friction).toStrictEqual(2);
+        expect(contactMaterial.restitution).toStrictEqual(3);
+        expect(contactMaterial.frictionRelaxation).toStrictEqual(4);
+        //@ts-ignore https://github.com/DefinitelyTyped/DefinitelyTyped/pull/69887
+        expect(contactMaterial.frictionStiffness).toStrictEqual(5);
+        expect(contactMaterial.relaxation).toStrictEqual(6);
+        expect(contactMaterial.stiffness).toStrictEqual(7);
+      });
     });
   });
 });
