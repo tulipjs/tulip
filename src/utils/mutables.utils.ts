278a01040b4c50fbc3f2464b6fd110f1c1abc353
pagoru <pagoru@gmail.com>
2024-08-08 19:18:44 +0200

    feat: Deploy to jsr - fix #229

--- src/utils/mutables.utils.ts
+++ src/utils/mutables.utils.ts
@@ -3,7 +3,7 @@ import { MutableFunction } from "../types";
 export const getValueMutableFunction = <Type>(
   data: MutableFunction<Type>,
   currentValue?: Type,
-): Promise<Type> | Type => {
+): Type => {
   return typeof data === "function"
     ? // @ts-ignore
       data(currentValue)
