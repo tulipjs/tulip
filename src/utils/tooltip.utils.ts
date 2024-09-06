2c6c6db9d6db853699c26681f094860c945a0b60
pagoru <pagoru@gmail.com>
2024-09-06 13:53:37 +0200

    fix: setCanvasTooltip is missing an explicit return type - fix #337

--- src/utils/tooltip.utils.ts
+++ src/utils/tooltip.utils.ts
@@ -1,7 +1,7 @@
 import { global } from "../global";
 import { isNotNullish } from "./nullish.utils";
 
-export const setCanvasTooltip = (title: string) => {
+export const setCanvasTooltip = (title: string): void => {
   const { canvas } = global.getApplication().$getApplication();
   isNotNullish(title)
     ? canvas.setAttribute("title", title)
